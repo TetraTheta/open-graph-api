fc39c3703d7761b3777915de3f06003c2ab8df47
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-05-28 13:51:58 +0900

    feat: Vercel Node.js Serverless Function 전환
    이거 하나 돌리겠다고 Next.js를 쓰는 건 좀 아닌 것 같다.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+//@ts-check
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig(js.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier);
